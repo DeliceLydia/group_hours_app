004c4cbe23f321e7fb799d4ea738a14fa9113396
delicelydia <Delydia84@gmail.com>
2021-02-10 18:16:28 +0200

    Add create new group page
    - Add new.html.erb file in the group/view folder
    Added a form with:
    - Field for name
    - Field for icon
    
    - Added index page for group page
    - styled group page

--- app/models/group.rb
+++ app/models/group.rb
@@ -3,8 +3,8 @@ class Group < ApplicationRecord
     belongs_to :user
     has_many :groups_hours
     has_many :groupings, through: :groups_hours
-    has_attached_file :icon, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
-    validates_attachment_content_type :icon, content_type: /\Aimage\/.*\z/
+    has_one_attached :icon
+    validates :icon, presence: true, blob: { content_type: ['image/jpg', 'image/jpeg', 'image/png'], size_range: 1..3.megabytes }
 
     def display_image
         icon.variant(resize_to_limit: [500, 500])
